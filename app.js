e7eec8fa7a9614a388efe6b69d405f458a612018
Bruno Disliler <bruno_disliler@outlook.com>
2022-08-16 12:16:33 -0300

    Adicionando rescue ao app.js e confirmando requisito 13 com 30% dos testes realizados

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 const Products = require('./controllers/ProductsController');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.get('/products', Products.getAll);
+app.get('/products', rescue(Products.getAll));
 
-app.get('/products/:id', Products.getById);
+app.get('/products/:id', rescue(Products.getById));
 
-app.post('/products', Products.create);
+app.post('/products', rescue(Products.create));
 
 // app.get('/sales', Products.getAllSales);
 
